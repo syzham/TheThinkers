385d9b1e64cf07a90b829ba555f964ea2ff0ac00
Syzham <syzham3@gmail.com>
2023-02-22 22:12:29 +0800

    Grid objects snaps to the closest grid position at start

--- Assets/Game/Scripts/Grids/GridObjects.cs
+++ Assets/Game/Scripts/Grids/GridObjects.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Game.Scripts.Grids
@@ -12,9 +11,26 @@ namespace Game.Scripts.Grids
 
         private void Start()
         {
-            var gridSize = GridManager.Instance.gridSize;
-            hitBox.size = new Vector2(height * gridSize, width * gridSize);
-            Debug.Log(height * gridSize);
+            var grid = GridManager.Instance;
+            // Creates the appropriate hitBox
+            var gridSize = grid.gridSize;
+            hitBox.size = new Vector2(width * gridSize, height * gridSize);
+
+            SnapToClosestGridPosition(new Vector2(grid.xOffset, grid.yOffset), gridSize);
+        }
+
+        /// <summary>
+        /// Snaps the grid object to the nearest grid position
+        /// </summary>
+        /// <param name="gridOffset"> the offset of the grid </param>
+        /// <param name="gridSize"> the size of each grid cell </param>
+        private void SnapToClosestGridPosition(Vector2 gridOffset, int gridSize)
+        {
+            var nearestCoord = (hitBox.bounds.min - new Vector3(gridOffset.x, gridOffset.y, 0)) / 32;
+            var snappedCoord = new Vector3(Mathf.RoundToInt(nearestCoord.x), Mathf.RoundToInt(nearestCoord.y), 0);
+            Vector3 centerOffset = hitBox.size / 2;
+            var snappedPosition = snappedCoord * gridSize + centerOffset;
+            transform.position = snappedPosition;
         }
     }
 }
